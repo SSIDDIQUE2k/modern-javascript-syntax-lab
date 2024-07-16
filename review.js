c4d86d2eb6d3a02e2d36530ebc945fdaa062aeee
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-07-16 10:54:37 -0400

    completed excercise 9 and 10

--- review.js
+++ review.js
@@ -199,4 +199,63 @@ function sentence(noun = 'cat', adjective = 'white') {
 
   // ---------------------------------------------------------------------------------------------------------------------------------------------------------//
 
-  
+  //Review: The ternary operator
+
+  // Convert the following `if...else` statement in to a ternary:
+
+let pizza = 'tasty';
+
+if (pizza === 'tasty') {
+  console.log('yum');
+} else {
+  console.log('yuck');
+}
+
+// Your code here
+
+// Review: The ternary operator
+const age = 22;
+let access = age < 21 ? 'Yes' : 'No';
+
+console.log(access); // 'Yes'
+
+//exercise 9
+
+const pizza3 = 'tasty';
+let taste = pizza3 === 'disgusting '? 'yum' : 'yuck';
+console.log(taste);
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+const LANG = localLangConfig || 'en';
+// Log the result
+console.log('Language setting:', LANG);
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+const USER_THEME = userSavedTheme || 'light';
+// Log the result
+console.log('User theme setting:', USER_THEME);
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+//Review: Optional chaining
+
